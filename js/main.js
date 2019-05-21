f2ace0e249dccbf2676dbe24c9598f16da291fd9
Helder <j_helder2@tss-hr420-dm01.fanshawec.ca>
2019-05-21 16:45:24 -0400

    more js

--- js/main.js
+++ js/main.js
@@ -11,21 +11,31 @@
 	// let stores the reference to element as a variable(in memory)
 	let svgGraphic = document.querySelector("#badgeSVG"),
 		mainHeadline = document.querySelector(".main-headline"),
-		subtitle1 = document.querySelector(".subtitle1");
+		subtitle1 = document.querySelector(".subtitle1"),
+		swapType = document.querySelector(".switch-type");
+
+
 	//functions are reusable pieces of code
 	//that you can run anytime.
 
 	function logMyId() {
 		console.log(this.id);
-
 		this.style.opacity = 0.5;
 	}
 
+	function swapText(){
+		mainHeadline.textContent = "Now you're something else";
+		mainHeadline.classList.toggle("selected");
+
+		subtitle1.textContent = "Something elssssssse";
+		subtitle1.classList.toggle("selected");
+	}
+
+
+//events always down here//
+
 	svgGraphic.addEventListener("click", logMyId);
+	swapType.addEventListener("click", swapText);
 
-	mainHeadline.textContent = "Now you're something else"
-
-	subtitle1.textContent = "Somthing different"
 
 })();
- 
